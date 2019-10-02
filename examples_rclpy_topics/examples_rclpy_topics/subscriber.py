94e47ece0156c66793a1a50cc8c3b6b0203ca311
AtsukiYokota <yokota@AtsukiYokotanoMacBook-Pro.local>
2019-10-02 14:21:14 +0900

    Add topic examples

---
+++ examples_rclpy_topics/examples_rclpy_topics/subscriber.py
@@ -0,0 +1,25 @@
+import rclpy
+from rclpy.node import Node
+
+from std_msgs.msg import String
+
+
+class MinimalSubscriber(Node):
+    def __init__(self):
+        super().__init__('minimal_subscriber')
+        self.subscription = self.create_subscription(
+            String, 'chatter', self.listener_callback)
+
+    def listener_callback(self, msg):
+        self.get_logger().info(msg.data)
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    minimal_subscriber = MinimalSubscriber()
+    rclpy.spin(minimal_subscriber)
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
